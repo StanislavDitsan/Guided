ab65de935e02b415ef17d08113fd13936ff84a5d
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-01-18 14:58:16 +0000

    Loaded active link tags and applied them

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('home', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('about.html', views.about, name='about'),
-    path('contact.html', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
