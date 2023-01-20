f163ece193ef2b26d5ee96659268494f41843c56
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-01-20 21:58:12 +0000

    Changed the order for urlpatterns

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('blog', views.PostList.as_view(), name='blog'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('home', views.home, name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
