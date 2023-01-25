c270ac61408c652f738e9b726eac9e21051aecee
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-01-25 14:45:33 +0000

    Wired url path for updated page

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('add_post', views.AddPostView.as_view(), name='add_post'),
+    path('post/<int:pk>/update',
+         views.UpdatePostView.as_view(),
+         name='update_post')
 ]
