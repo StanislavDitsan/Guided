b5a0cb77fec2a3cfdd117f3ec747f42ca4516dcd
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-01-29 21:52:34 +0000

    Set up timeout function for message

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,26 @@
 function autoType(element, delay) {
-    var p = document.querySelector(element);
-    var i = 0;
-    var text = p.textContent;
-    p.textContent = "";
-  
-    function type() {
-      if (i < text.length) {
-        p.textContent += text.charAt(i);
-        i++;
-        setTimeout(type, delay);
-      }
+  var p = document.querySelector(element);
+  var i = 0;
+  var text = p.textContent;
+  p.textContent = "";
+
+  function type() {
+    if (i < text.length) {
+      p.textContent += text.charAt(i);
+      i++;
+      setTimeout(type, delay);
     }
-  
-    setTimeout(type, delay);
   }
-  
-  autoType('.type-script', 100);
+
+  setTimeout(type, delay);
+}
+
+autoType('.type-script', 100);
+
+
+setTimeout(function () {
+  let messages = document.getElementById("msg");
+  let alert = new bootstrap.Alert(messages);
+  alert.close();
+
+}, 3000);
