3df1830e6a0ee47af9c4a21375c890dbbf8b90a2
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-01-31 14:21:08 +0000

    Updated urls for contact function

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from . import views
 from django.urls import path
 from .views import CategoryView, search, delete_comment
+from .views import contact, success
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog', views.PostList.as_view(), name='blog'),
     path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
+    path('contact/', contact, name='contact'),
+    path('success/', success, name='success'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('add_post', views.AddPostView.as_view(), name='add_post'),
@@ -18,7 +20,5 @@ urlpatterns = [
          name='delete_post'),
     path('category/<str:category>/', CategoryView, name='category'),
     path('search', views.search, name='search'),
-    path('post/<int:pk>/delete/',
-         views.delete_comment,
-         name='delete_comment'),
+    path('post/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
 ]
