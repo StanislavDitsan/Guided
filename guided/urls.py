ebd0f2ed018263a9aa88f0d30e0bbf4fc809556d
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-02-02 13:18:48 +0000

    Added handler404 to urls.py

--- guided/urls.py
+++ guided/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler400
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +8,6 @@ urlpatterns = [
     path('', include('blog.urls'), name='blog_urls'),
     path('accounts/', include('allauth.urls')),
 ]
+
+
+handler404 = 'blog.views.page_404'
